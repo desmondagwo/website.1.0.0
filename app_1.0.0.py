4c78a810360b4198f5616231cdaf7f80dc2ed8db
desmondagwo <agwodesmondn@gmailcom>
2024-07-03 17:12:45 -0400

    updated flask project

--- app_1.0.0.py
+++ app_1.0.0.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
-def home():
-    return '''<p>HELLO WORLD</p>'''
+def login():
+    return render_template("login.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug=True)
+
+
