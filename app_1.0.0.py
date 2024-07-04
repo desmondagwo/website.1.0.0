aa7a15cdc4ca4e7c589315eb19c2da9b96231a52
desmondagwo <agwodesmondn@gmailcom>
2024-07-04 14:04:15 -0400

    updated flask project

--- app_1.0.0.py
+++ app_1.0.0.py
@@ -1,5 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
+Jobs = [
+    {
+    'id' : 1,
+    'title' : 'splunk admin',
+    'salary': '$90/hr',
+    'location': 'NC, USA'
+    },
+    {
+    'id' : 2,
+    'title' : 'splunk developer',
+    'salary': '$60/hr',
+    'location': 'NJ, USA'
+    },
+    {
+    'id' : 3,
+    'title' : 'software engineer',
+    'salary': '',
+    'location': 'Tx, USA'
+    }
+]
 
 @app.route('/')
 def home():
@@ -12,6 +33,15 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template("signup.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html", jobs=Jobs)
+
+@app.route('/api/jobs')
+def jobs():
+    return jsonify(Jobs)
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug=True)
 
